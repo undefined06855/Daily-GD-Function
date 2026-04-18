d380cb83ee0d9526e9a113c702369d313ffaaad2
undefined06855 <currentcashew57@gmail.com>
2026-04-19 00:36:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Daily GD Function
+
+This simply displays a unique GD function every day, with its address, what platforms it's on and how many times it's
+shown up before!
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.3.5. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
---
+++ main.js
@@ -0,0 +1,61 @@
+import { Temporal } from "@js-temporal/polyfill";
+import * as jsc from "bun:jsc"
+
+async function main() {
+    console.log("Fetching codegen data...");
+    let res = await fetch("https://prevter.github.io/bindings-meta/CodegenData-2.2081.json");
+    let data = (await res.json()).classes;
+    let functions = [];
+    let classes = [];
+
+
+    for (let classData of data) {
+        let split = classData.name.split("::");
+        let namespace, className;
+        if (split.length == 2) {
+            namespace = split[0];
+            className = split[1];
+        } else {
+            namespace = "";
+            className = split[0];
+        }
+
+        for (let functionData of classData.functions) {
+            functions.push({
+                ...functionData,
+                namespace, className
+            });
+        }
+
+        classes.push(className);
+    }
+
+    Bun.serve({
+        routes: {
+            "/": async req => {
+                let template = await Bun.file("index.html").text();
+                let functionNumber = Temporal.Now.plainDateISO().since(Temporal.PlainDate.from("2026-04-14"), { largestUnit: "days" });
+
+                let days = functionNumber.total("days");
+                jsc.setRandomSeed(Number(Bun.hash(days.toString())));
+                let index = ~~(Math.random() * functions.length);
+                let todayFunction = functions[index];
+
+                return new Response(
+                    template.replace("/* filled by server */", `today=${JSON.stringify(todayFunction)}; classes=${JSON.stringify(classes)}; index=${index}; days=${days}; `),
+                    {
+                        headers: { "Content-Type": "text/html" }
+                    }
+                );
+            },
+
+            "/style.css": Bun.file("style.css")
+        },
+
+        port: 2002
+    });
+
+    console.log("Hosting Daily GD Function on port 2002");
+}
+
+main();
